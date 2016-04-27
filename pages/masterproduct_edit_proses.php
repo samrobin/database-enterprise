47024a64f5cc9f57da656bd411b97f3b565d05e8
liabonita <liabonita6196@gmail.com>
2016-04-27 21:19:27 +0700

    dikit
    lia

--- pages/masterproduct_edit_proses.php
+++ pages/masterproduct_edit_proses.php
@@ -18,7 +18,7 @@ if(check_file_extension($img['name'], $img_ext)){
 			$sumber = $img['tmp_name'];
 			$tujuan = '../pages/images/product/'.$_GET['id'].'.jpg';
 			move_uploaded_file($sumber, $tujuan);
-			$sql = "UPDATE dokter SET kode_bar='$kode',nama_bar='$nama', hargajual_bar='$hargajual', hargabeli_bar='$hargabeli',
+			$sql = "UPDATE master_barang SET kode_bar='$kode',nama_bar='$nama', hargajual_bar='$hargajual', hargabeli_bar='$hargabeli',
 			foto_bar='$tujuan' WHERE id_bar = $_GET[id]";
 			echo $sql;
 			mysqli_query($k,$sql);
